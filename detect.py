2925492208c4fe2ffae8715c21f661acd8001fe6
jira-edu <jirapong.edu@gmail.com>
2025-11-17 12:07:44 +0700

    first prototype

--- detect.py
+++ detect.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("/home/jiji/Desktop/best.pt")
+model = YOLO("/home/jiji/Desktop/best2.pt")
 
 
-image_path = "/home/jiji/Desktop/dataset/LINE_ALBUM_14112025_251115_473.jpg"
+image_path = "/home/jiji/Desktop/dataset/LINE_ALBUM_14112025_251115_1.jpg"
 img = cv2.imread(image_path)
 
 results = model(img, conf=0.5)
@@ -22,7 +22,22 @@ for box in boxes:
     # วาดกรอบ
     cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
+
+cv2.putText(
+    img,
+    str(count),
+    (80, 250),  # ตำแหน่ง (x, y)
+    cv2.FONT_HERSHEY_SIMPLEX,
+    7.0,       # ขนาดฟอนต์
+    (0, 0, 255),  # สีแดง (B,G,R)
+    15,         # ความหนาเส้น
+    cv2.LINE_AA
+)
+
 print("จำนวนไม้:", count)
+
+cv2.namedWindow("Detected Sticks", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("Detected Sticks", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow("Detected Sticks", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
